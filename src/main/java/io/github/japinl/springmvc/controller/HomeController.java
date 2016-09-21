e8cf654c1c99b8567de61bc0f17465dd857451f2
japinl <japinli@hotmail.com>
2016-09-21 23:14:05 +0800

    init spring mvc project

---
+++ src/main/java/io/github/japinl/springmvc/controller/HomeController.java
@@ -0,0 +1,17 @@
+package io.github.japinl.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("greeting", "Welcome to Spring Test");
+		model.addAttribute("tagline", "The one and only amazing Spring Test");
+		
+		return "welcome";
+	}
+}
